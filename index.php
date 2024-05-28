39e5ba4bfed095aba6c7e6a309ff79050c83e9b7
ezyinfo <65986425+ezyinfo@users.noreply.github.com>
2024-05-28 16:09:00 +0200

    Add Frontend

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- FIXME: Add meta desc for SEO -->
+    <title>Breco</title>
+</head>
+
+<body>
+
+    <header>
+
+        <img src="assets/images/back.png" alt="Image de fond décorative">
+        <div id="banner">
+            <nav>
+                <img src="assets/images/burger.svg" alt="Menu">
+            </nav>
+            <h1>Breco</h1>
+        </div>
+    </header>
+
+    <main class="full_width flex">
+        <h2>Bienvenue</h2>
+
+        <a class="btn c2a" href="login.php">Login</a>
+        <a class="btn" href="register.php">Register</a>
+
+        <!-- No footer for this project -->
+
+        <script src="js/script.js"></script>
+    </main>
+</body>
+
+</html>
